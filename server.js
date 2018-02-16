ae1b6a33e5143d418b1a091d1731c7717eaecb70
Sheena Mattu <sheenamattu04@gmail.com>
2018-02-16 13:27:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
     //res.send('Article one requested and will be served here')
    // res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-   res.send(createTemplate(articleOne,))
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
