90c6f0a568e0d631c8e6cca3d1a6004b334c2109
Sheena Mattu <sheenamattu04@gmail.com>
2018-02-18 13:41:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML = "Using client side Javascript now!"
 
 var img = document.getElemntById("madi");
 img.onclick = function(){
-    img.style.boxShadow = "3px 3px 3px black" ;
+    img.style.boxShadow = "30px 30px 30px black" ;
 }
