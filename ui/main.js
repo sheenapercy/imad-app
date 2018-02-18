18def4caf5966621f7a90542fe3c6362bc4edecc
Sheena Mattu <sheenamattu04@gmail.com>
2018-02-18 13:42:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 alert("Hello!Thanks for visiting")
 console.log('Loaded!');
 
-var element = document.getElementById ("main-text");
+var element = document.getElementById("main-text");
 element.innerHTML = "Using client side Javascript now!"
 
-var img = document.getElemntById("madi");
+var img = document.getElementById("madi");
 img.onclick = function(){
     img.style.boxShadow = "30px 30px 30px black" ;
 }
