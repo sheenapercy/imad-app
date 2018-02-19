ee8109f9f22a8f9e494e0405304bbd657dc4ae5e
Sheena Mattu <sheenamattu04@gmail.com>
2018-02-19 18:47:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,14 +19,23 @@ img.onclick = function(){
 */
 
 var button = document.getElementById("counter");
-var counter = 0;
+
 button.onclick = function(){
-    //make request
+    //create request
+    var request = new XMLHttpRequest();
     
     //capture response and store it
-    
-    //render var in correct span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //check if request was successfully completed
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //make request
+    request.open('GET','http://sheenamattu04.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
