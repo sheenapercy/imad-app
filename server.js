ba2986efa41430d76502122f141cb07027a72399
Sheena Mattu <sheenamattu04@gmail.com>
2018-02-21 17:32:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,8 +121,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name/:name',function(req,res){
    //get name from request
-   var name;
-   names.push=req.params.name;
+   var name = req.params.name;
    //JSON Javascript Object Notation
    
    res.send(JSON.stringify(names));
